fcb4b1ac41a6006b391f4a6d68b262ac9d02882f
darlenecoelho <darle2015@gmail.com>
2023-07-26 15:09:44 -0300

    Adicionar arquivos de projeto.

---
+++ PoC.SimpleEmailApp/Services/EmailService/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using PoC.SimpleEmailApp.Models;
+
+namespace PoC.SimpleEmailApp.Services.EmailService
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
